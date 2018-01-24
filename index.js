83962efdadf438c898f2d9bd5da82eff9113d0a3
Suni <suni.suni571@gmail.com>
2018-01-24 14:47:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(array1,array2){
+  var array=[];
+  for(let i=0; i<array1.length;i++){
+   array[i] = array1[i]+ ' plays' + array2[i];
+  }
+  return array;
+}
+
+function johnLennonFacts(array){
+  var i=0;
+  while(i < array.length){
+    array[i]= array[i]+"!!!";
+    i++;
+  }
+  return array;
+}
