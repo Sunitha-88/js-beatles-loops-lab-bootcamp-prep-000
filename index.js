787f532468a18cd40543e0aa3b39d78169f8e89d
Suni <suni.suni571@gmail.com>
2018-01-24 14:52:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(array1,array2){
   var array=[];
   for(let i=0; i<array1.length;i++){
-   array[i] = array1[i]+ ' plays' + array2[i];
+   array[i] = array1[i]+ ' plays ' + array2[i];
   }
   return array;
 }
